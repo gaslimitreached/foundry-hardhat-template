7c5a2a759dd42baa9c745478db8ff1fb354a78e0
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2022-02-28 14:07:53 -0600

    chore: use forge-std Vm

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ build:
 lint:
 	solhint ./src/**.sol
 test:
-	forge test -vvv
+	forge test -vvvv
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
+import "forge-std/Vm.sol";
 
 contract ContractTest is DSTest {
+    Vm public vm = Vm(HEVM_ADDRESS);
     function setUp() public {}
 
     function testExample() public {
